616a83912c0dc7e83e3ce071dd127ac958c82bfd
ravigupta <rg187888@gmail.com>
2025-05-31 18:36:35 +0530

    add moving snake animation in snake page

--- src/pages/Snake.jsx
+++ src/pages/Snake.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { motion } from "framer-motion";
 const Snake = () => {
-  document.title = "BR-Games | Snake"
+  document.title = "BR-Games | Snake";
   return (
     <div className="w-full min-h-[100vh] bg-black/85 dark:bg-white/65 dark:text-black/75 text-white/75 flex flex-col justify-center items-center transition-all duration-800 relative px-16">
       <div className="absolute w-full top-0 left-0 z-10">
@@ -71,14 +71,116 @@ const Snake = () => {
               </motion.span>
             </motion.h1>
           </div>
+          <div className="w-[40%] h-full overflow-hidden flex items-center relative">
+            <div className="w-full h-[90%] flex gap-4 relative overflow-hidden">
+              <motion.div
+                initial={{ y: "-100%", opacity: 0.4 }}
+                animate={{ y: "250%", opacity: 1 }}
+                transition={{
+                  duration: 5,
+                  ease: "linear",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="w-[5vw] ml-5 mb-0.5 h-[35vh] flex items-end justify-between bg-linear-to-t/increasing from-pink-500 to-pink-200 shadow-md rounded-full px-3.5 py-4"
+              >
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+              </motion.div>
+              <motion.div
+                initial={{ y: "-100%", opacity: 0.4 }}
+                animate={{ y: "250%", opacity: 1 }}
+                transition={{
+                  duration: 5,
+                  delay: 2,
+                  ease: "linear",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="w-[5vw] mb-0.5 h-[35vh] flex items-end justify-between bg-linear-to-t/oklch from-pink-500 to-pink-100 shadow-md rounded-md px-3 py-1"
+              >
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+              </motion.div>
+              <motion.div
+                initial={{ y: "250%", opacity: 0.4 }}
+                animate={{ y: "-100%", opacity: 1 }}
+                transition={{
+                  duration: 5,
+                  ease: "linear",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="w-[5vw] ml-5 mb-0.5 h-[35vh] flex items-start justify-between bg-linear-to-t/increasing from-pink-500 to-pink-200 shadow-md rounded-full px-3.5 py-4"
+              >
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+              </motion.div>
+              <motion.div
+                initial={{ y: "250%", opacity: 0.4 }}
+                animate={{ y: "-100%", opacity: 1 }}
+                transition={{
+                  duration: 5,
+                  delay: 2,
+                  ease: "linear",
+                  repeat: Infinity,
+                  repeatType: "loop",
+                }}
+                className="w-[5vw] ml-5 mb-0.5 h-[35vh] flex items-start justify-between bg-linear-to-t/oklch from-pink-500 to-pink-100 shadow-md rounded-md px-3 py-1"
+              >
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+                <span className="inline-flex items-center justify-center px-0.5 w-[1vw] mb-0.5 h-[2vh] bg-black shadow-md rounded-full ">
+                  <span className="inline-block w-[0.5vw] h-[1vh] bg-white shadow-md rounded-full"></span>
+                </span>
+              </motion.div>
+            </div>
+          </div>
         </div>
         <motion.p
-          initial={{ x: "-100%", opacity: 0.2 }}
+          initial={{ x: "-100%", opacity: 0.4 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 2, ease: "linear" }}
-          className="text-[3.5vw] pt-2 mt-2 w-full h-fit  leading- text-center tracking-widest font-[kajiro] relative"
+          className="text-[3.5vw] w-full h-24 gap-2 flex items-center justify-center text-shadow-lg leading-0 text-center tracking-widest font-[kajiro] relative"
         >
-          Challenge your brain in this classic battle of Xs and Os.
+          <motion.span
+            initial={{ x: -100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.4, delay: 3, ease: "backInOut" }}
+            className="inline-block pt-3"
+          >
+            Challenge your brain in
+          </motion.span>
+          <motion.span
+            initial={{ scaleX: 0, opacity: 0 }}
+            animate={{ scaleX: 1, opacity: 1 }}
+            transition={{ duration: 1.4, delay: 3.5, ease: "backInOut" }}
+            className="inline-flex h-[12vh] pl-1 mt-2 pt-1 w-[6vw] leading-0 items-center justify-center rounded-full bg-[#FFB921] "
+          >
+            this
+          </motion.span>
+          <motion.span
+            initial={{ x: 100, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1.4, delay: 3, ease: "backInOut" }}
+            className="inline-block pt-3"
+          >
+            classic battle of Xs and Os.
+          </motion.span>
+          {/* Challenge your brain in this classic battle of Xs and Os. */}
         </motion.p>
       </div>
     </div>
